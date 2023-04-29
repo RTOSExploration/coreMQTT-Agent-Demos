a98632064aacfd5a4ab6177d649cfec794f76276
Mingjie Shen <shen497@purdue.edu>
2023-04-29 20:13:51 +0000

    update

--- gen-wllvm.sh
+++ gen-wllvm.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-DB=$RTOSExploration/bitcode-db/coreMQTT-Agent
-rm -rf $DB
+#DB=$RTOSExploration/bitcode-db/coreMQTT-Agent
+#rm -rf $DB
+
+export LLVM_COMPILER=hybrid
+export LLVM_COMPILER_PATH=/usr/lib/llvm-14/bin
+export BINUTILS_TARGET_PREFIX=arm-none-eabi
 
 cd build/Cortex-M3_MPS2_QEMU_GCC
 make clean
-genbc $DB -c "make -j"
+make -j$(nproc) CC=wllvm
+extract-bc output/RTOSDemo.elf
